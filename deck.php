dcd85c0ff709ce01a62daada87390730cfad8e94
linuxmania <linuxmania@users.noreply.github.com>
2025-03-22 19:12:54 -0400

    Add files via upload

---
+++ deck.php
@@ -0,0 +1,306 @@
+<?php
+
+/***************************
+*
+* REST Get to return a shuffled deck
+* Copyleft (C) 2025 Daniel K. Spicer
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************/
+
+class Card {
+  public $name;
+  public $suit;
+  public $value;
+}    
+    
+$card1 = new Card();
+$card1->name = "King";
+$card1->suit = "Diamonds";
+$card1->value = "13";
+
+$card2 = new Card();
+$card2->name = "Queen";
+$card2->suit = "Diamonds";
+$card2->value = "12";
+
+$card3 = new Card();
+$card3->name = "Jack";
+$card3->suit = "Diamonds";
+$card3->value = "11";
+
+$card4 = new Card();
+$card4->name = "Ten";
+$card4->suit = "Diamonds";
+$card4->value = "10";
+
+$card5 = new Card();
+$card5->name = "Nine";
+$card5->suit = "Diamonds";
+$card5->value = "9";
+
+$card6 = new Card();
+$card6->name = "Eight";
+$card6->suit = "Diamonds";
+$card6->value = "8";
+
+$card7 = new Card();
+$card7->name = "Seven";
+$card7->suit = "Diamonds";
+$card7->value = "7";
+
+$card8 = new Card();
+$card8->name = "Six";
+$card8->suit = "Diamonds";
+$card8->value = "6";
+
+$card9 = new Card();
+$card9->name = "Five";
+$card9->suit = "Diamonds";
+$card9->value = "5";
+
+$card10 = new Card();
+$card10->name = "Four";
+$card10->suit = "Diamonds";
+$card10->value = "4";
+
+$card11 = new Card();
+$card11->name = "Three";
+$card11->suit = "Diamonds";
+$card11->value = "3";
+
+$card12 = new Card();
+$card12->name = "Two";
+$card12->suit = "Diamonds";
+$card12->value = "2";
+
+$card13 = new Card();
+$card13->name = "Ace";
+$card13->suit = "Diamonds";
+$card13->value = "1";
+
+
+$card14 = new Card();
+$card14->name = "King";
+$card14->suit = "Spades";
+$card14->value = "13";
+
+$card15 = new Card();
+$card15->name = "Queen";
+$card15->suit = "Spades";
+$card15->value = "12";
+
+$card16 = new Card();
+$card16->name = "Jack";
+$card16->suit = "Spades";
+$card16->value = "11";
+
+$card17 = new Card();
+$card17->name = "Ten";
+$card17->suit = "Spades";
+$card17->value = "10";
+
+$card18 = new Card();
+$card18->name = "Nine";
+$card18->suit = "Spades";
+$card18->value = "9";
+
+$card19 = new Card();
+$card19->name = "Eight";
+$card19->suit = "Spades";
+$card19->value = "8";
+
+$card20 = new Card();
+$card20->name = "Seven";
+$card20->suit = "Spades";
+$card20->value = "7";
+
+$card21 = new Card();
+$card21->name = "Six";
+$card21->suit = "Spades";
+$card21->value = "6";
+
+$card22 = new Card();
+$card22->name = "Five";
+$card22->suit = "Spades";
+$card22->value = "5";
+
+$card23 = new Card();
+$card23->name = "Four";
+$card23->suit = "Spades";
+$card23->value = "4";
+
+$card24 = new Card();
+$card24->name = "Three";
+$card24->suit = "Spades";
+$card24->value = "3";
+
+$card25 = new Card();
+$card25->name = "Two";
+$card25->suit = "Spades";
+$card25->value = "2";
+
+$card26 = new Card();
+$card26->name = "Ace";
+$card26->suit = "Spades";
+$card26->value = "1";
+
+
+$card27 = new Card();
+$card27->name = "King";
+$card27->suit = "Clubs";
+$card27->value = "13";
+
+$card28 = new Card();
+$card28->name = "Queen";
+$card28->suit = "Clubs";
+$card28->value = "12";
+
+$card29 = new Card();
+$card29->name = "Jack";
+$card29->suit = "Clubs";
+$card29->value = "11";
+
+$card30 = new Card();
+$card30->name = "Ten";
+$card30->suit = "Clubs";
+$card30->value = "10";
+
+$card31 = new Card();
+$card31->name = "Nine";
+$card31->suit = "Clubs";
+$card31->value = "9";
+
+$card32 = new Card();
+$card32->name = "Eight";
+$card32->suit = "Clubs";
+$card32->value = "8";
+
+$card33 = new Card();
+$card33->name = "Seven";
+$card33->suit = "Clubs";
+$card33->value = "7";
+
+$card34 = new Card();
+$card34->name = "Six";
+$card34->suit = "Clubs";
+$card34->value = "6";
+
+$card35 = new Card();
+$card35->name = "Five";
+$card35->suit = "Clubs";
+$card35->value = "5";
+
+$card36 = new Card();
+$card36->name = "Four";
+$card36->suit = "Clubs";
+$card36->value = "4";
+
+$card37 = new Card();
+$card37->name = "Three";
+$card37->suit = "Clubs";
+$card37->value = "3";
+
+$card38 = new Card();
+$card38->name = "Two";
+$card38->suit = "Clubs";
+$card38->value = "2";
+
+$card39 = new Card();
+$card39->name = "Ace";
+$card39->suit = "Clubs";
+$card39->value = "1";
+
+
+$card40 = new Card();
+$card40->name = "King";
+$card40->suit = "Hearts";
+$card40->value = "13";
+
+$card41 = new Card();
+$card41->name = "Queen";
+$card41->suit = "Hearts";
+$card41->value = "12";
+
+$card42 = new Card();
+$card42->name = "Jack";
+$card42->suit = "Hearts";
+$card42->value = "11";
+
+$card43 = new Card();
+$card43->name = "Ten";
+$card43->suit = "Hearts";
+$card43->value = "10";
+
+$card44 = new Card();
+$card44->name = "Nine";
+$card44->suit = "Hearts";
+$card44->value = "9";
+
+$card45 = new Card();
+$card45->name = "Eight";
+$card45->suit = "Hearts";
+$card45->value = "8";
+
+$card46 = new Card();
+$card46->name = "Seven";
+$card46->suit = "Hearts";
+$card46->value = "7";
+
+$card47 = new Card();
+$card47->name = "Six";
+$card47->suit = "Hearts";
+$card47->value = "6";
+
+$card48 = new Card();
+$card48->name = "Five";
+$card48->suit = "Hearts";
+$card48->value = "5";
+
+$card49 = new Card();
+$card49->name = "Four";
+$card49->suit = "Hearts";
+$card49->value = "4";
+
+$card50 = new Card();
+$card50->name = "Three";
+$card50->suit = "Hearts";
+$card50->value = "3";
+
+$card51 = new Card();
+$card51->name = "Two";
+$card51->suit = "Hearts";
+$card51->value = "2";
+
+$card52 = new Card();
+$card52->name = "Ace";
+$card52->suit = "Hearts";
+$card52->value = "1";
+
+
+$cards = array($card1,$card2,$card3,$card4,$card5,$card6,$card7,$card8,$card9,$card10,$card11,$card12,$card13,$card14,$card15,$card16,$card17,$card18,$card19,$card20,$card21,$card22,$card23,$card24,$card25,$card26,$card27,$card28,$card29,$card30,$card31,$card32,$card33,$card34,$card35,$card36,$card37,$card38,$card39,$card40,$card41,$card42,$card43,$card44,$card45,$card46,$card47,$card48,$card49,$card50,$card51,$card52);
+
+$shuffled = array();
+
+while(sizeof($cards) > 0){
+    $index = rand(0,sizeof($cards) - 1);
+    $shuffled[] = $cards[$index];
+    array_splice($cards, $index, 1);
+}
+
+    
+echo json_encode($shuffled);    
+
+?>
